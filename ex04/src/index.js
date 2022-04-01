0c7819e96a86be536c7d863a702f5f04e6be281c
domuz86 <muamer.m86@gmail.com>
2022-04-01 09:15:24 +0200

    Day04

---
+++ ex04/src/index.js
@@ -0,0 +1,21 @@
+function main(){
+
+    
+    //Only change code below this line
+    var myDecimal=23.15;
+    var myOtherDecimal=254e-5;
+
+    //Only change code above this line
+        
+    return {
+        myDecimal,
+        myOtherDecimal
+        
+    };
+
+    }
+
+ 
+console.log(main());
+
+module.exports = main;
